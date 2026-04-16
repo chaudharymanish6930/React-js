ae0027b3628fd94fba0e2898bb3cade6be8c9556
Manish Choudhary <me.developer4u@gmail.com>
2026-04-16 12:54:57 +0530

    Basics of React js

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>hello</div>
+  )
+}
+
+function rohit(){
+   return ( 
+    <h1>"Manish"</h1>)
+}
+
+const Manish = ()=>{
+  return "Hello Manish"
+}
+
+const fun = ()=>{
+  return (
+  <>
+  <div>MAster</div>
+  <h1>Hello bhai</h1>
+  <div>
+    <h1>JI hain kaise hi aap log</h1>
+  </div>
+  </>
+  )
+}
+
+// export default App
+// export default rohit
+// export default Manish;
+export default fun;
