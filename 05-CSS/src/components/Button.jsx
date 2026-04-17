2ad75c233a09ee5db26a131e2166d63cb64b3b58
Manish Choudhary <me.developer4u@gmail.com>
2026-04-17 13:58:31 +0530

    CSS Added by the .Css Files

---
+++ 05-CSS/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Button.css'
+
+const button = () => {
+  return (
+    <div className='btn'>
+        this is my button
+    </div>
+  )
+}
+
+export default button
---
+++ 05-CSS/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Header.css'
+
+const header = () => {
+  return (
+    <div className='header'>
+        <h2>Github Analyzer</h2>
+        <button className='btn'>Login</button>
+    </div>
+  )
+}
+
+export default header
