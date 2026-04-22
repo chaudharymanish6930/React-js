e624b1843a2a7b48b85445c2d5835b6a7af1853e
Manish Choudhary <me.developer4u@gmail.com>
2026-04-23 04:33:45 +0530

    Practice for UseState

---
+++ Practice/src/App.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [first, setfirst] = useState("");
+  const [second, setsecond] = useState('');
+  const [arr, setarr] = useState([]);
+
+  const submit=(e)=>{
+    e.preventDefault();
+    console.log(first);
+    console.log(second)
+
+    const copy=[...arr];
+    copy.push({first, second})
+
+    setarr(copy)
+
+    setfirst('')
+    setsecond("")
+  }
+
+
+  const deleteNote = (idx) => {
+    const copyTask = [...arr];
+
+    copyTask.splice(idx, 1)
+
+    setarr(copyTask)
+  }
+
+
+  return (
+    <div>
+      <form 
+      onSubmit={(e)=>{
+        submit(e)
+      }}>
+      <h1>Add Notes</h1>
+      <input type="text" 
+      placeholder='likhoo'
+      onChange={(e)=>{
+        setfirst(e.target.value);
+        // console.log(e.target.value)
+      }}
+       />
+
+      <input type="text"
+      placeholder='dubara llikho'
+      onChange={(e)=>{
+        setsecond(e.target.value);
+        // console.log(e.target.value)
+      }}
+       />
+      
+      <button
+      className='bg-white active:scale-95 font-medium w-full outline-none  text-black px-5 py-2 rounded'>
+        Add note
+      </button>
+
+      </form>
+
+      {arr.map(function (elem, idx) {
+            return <div key={idx} >
+              <div>
+                <h3 >{elem.first}</h3>
+                <p >{elem.second}</p>
+              </div>
+              <button onClick={() => {
+                deleteNote(idx)
+              }} >Delete</button>
+            </div>
+          })}
+
+      </div>
+  )
+}
+
+export default App
