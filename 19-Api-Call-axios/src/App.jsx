39819035b1472baea4dc6d06e17863d64bba1050
Manish Choudhary <me.developer4u@gmail.com>
2026-04-24 23:42:39 +0530

    Api calling by the axios

---
+++ 19-Api-Call-axios/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import axios from 'axios';
+
+const App = () => {
+
+  const getData= async () => {
+    // const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+    // console.log(response)
+    // const {response} = await axios.get('https://jsonplaceholder.typicode.com/users')
+    // console.log(response)
+
+    const data = await axios.get('https://picsum.photos/v2/list')
+    console.log(data.data)
+  }
+  return (
+    <div>
+      <button onClick={getData}> Get Data</button>
+    </div>
+  )
+}
+
+export default App
