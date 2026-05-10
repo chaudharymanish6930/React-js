062c648934d0ac2e0f87bef2d8415e701521af72
Manish Choudhary <me.developer4u@gmail.com>
2026-05-10 22:00:13 +0530

    Practice Routing

---
+++ 26-Practice-Routes/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Contact from './pages/contact'
+import Navbar from './compnents/navbar'
+
+const App = () => {
+  return (
+    <div>
+      {/* <h1>Full the content</h1>  common for all the webpages    */}
+      <h2>
+        <h3>This is Common for all Pages</h3>
+        <a href="/">Home</a>
+        <a href="/about">About Us</a>
+        <a href="/contact">Contact</a>
+      </h2>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App  
---
+++ 26-Practice-Routes/src/compnents/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+        <nav>
+            <a href="/">home</a>
+            <a href="/about">About us</a>
+            <a href="/contact">Contact </a>
+        </nav>
+    </div>
+  )
+}
+
+export default navbar
